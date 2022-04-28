070ad77f88809231feb46f421a5a13aae637b9cb
Dominic Sore <domcsore@googlemail.com>
2022-04-28 21:49:40 +0100

    Initial frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './routes/index';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+export default App;
---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react';
+
+const Home: FunctionComponent = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welcome home</p>
+    </div>
+  )
+}
+
+export default Home;
