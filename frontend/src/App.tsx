86fd1aba21f12d177b18825dd8ed850c1d446154
Dominic Sore <domcsore@googlemail.com>
2022-05-07 10:21:40 +0100

    added category name input

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import CreateCategory from './routes/createCategory';
 
 import Home from './routes/index';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/create-category" element={<CreateCategory />} />
       </Routes>
     </div>
   );
--- frontend/src/components/PageWrapper.tsx
+++ frontend/src/components/PageWrapper.tsx
@@ -1,14 +1,20 @@
 import React, { FunctionComponent, PropsWithChildren } from 'react';
+import styled from 'styled-components';
+import { spacing } from '../assets/styles';
 
 import Header from './Header';
+
+const Main = styled.main`
+  padding: 0 ${spacing.lg} 0 ${spacing.lg};
+`;
 
 const PageWrapper: FunctionComponent<PropsWithChildren<{}>> = ({children}) => {
   return (
     <React.Fragment>
       <Header />
-      <main>
+      <Main>
         {children}
-      </main>
+      </Main>
     </React.Fragment>
   )
 }
